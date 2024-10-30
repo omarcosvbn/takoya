47c56f61fda6d5abe9e00696394b8612e47badc1
omarcosvbn <omarcosvbn@gmail.com>
2024-10-30 04:24:30 +0000

    homepage base 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,104 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <header className={styles.header}>
+        <img className={styles.logo} src="/takoya.svg" alt="Takoya" />
+      </header>
+      <nav className={styles.nav}>
+        <h1>Menu</h1>
+        <h1>Restaurant</h1>
+        <h1>Delivery</h1>
+      </nav>
+      <main>
+        <div className={styles.steamy}>
+          <img className={styles.steam1} src="/steam.svg" alt="Steam" />
+          <img className={styles.steam2} src="/steam.svg" alt="Steam" />
+          <img className={styles.steam3} src="/steam.svg" alt="Steam" />
+        </div>
+        <img className={styles.takoyaki} src="/takoyaki.svg" alt="Takoyaki" />
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+        <p>a</p>
+      </main>
+    </>
   );
 }
